9a9a24ab598d70f9b06d7ba479d9aac7f4fc53f9
Jeyaruban jenushan <jjenushan550@gmail.com>
2024-12-16 23:38:53 +0530

    first init

---
+++ src/test/java/org/example/fuel_management_system/FuelManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.fuel_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuelManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
