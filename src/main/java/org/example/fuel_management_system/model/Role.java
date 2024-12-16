f81f8c55a9fd3b623984fbf6379116f38d9621ed
holibhai <mohanathasholins17@gmail.com>
2024-12-17 00:58:45 +0530

    UserAcoount Entity Created with Role and databse code changes

---
+++ src/main/java/org/example/fuel_management_system/model/Role.java
@@ -0,0 +1,7 @@
+package org.example.fuel_management_system.model;
+
+public enum Role {
+    VEHICLE_OWNER,
+    FUELSTATION_OWNER,
+    ADMIN
+}
---
+++ src/main/java/org/example/fuel_management_system/model/UserAccount.java
@@ -0,0 +1,34 @@
+package org.example.fuel_management_system.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "UserAccount")
+public class UserAccount {
+    @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="Id")
+    private int UserId;
+
+    @Column(name="NIC")
+    private String NIC;
+
+    @Column(name = "TelNo")
+    private String TelNo;
+
+    @Column(name="FirstName")
+    private String FirstName;
+
+    @Column(name = "LastName")
+    private String LastName;
+
+    @Column(name="UserName")
+    private String UserName;
+
+    @Column(name="Password")
+    private String Password;
+
+    @Enumerated(value=EnumType.STRING)
+    private Role Role;
+
+}
