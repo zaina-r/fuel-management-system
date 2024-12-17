c14b3dad4b294d2b537ea4298cdc2806d7342070
Jeyaruban jenushan <jjenushan550@gmail.com>
2024-12-17 17:14:08 +0530

    JwtAuthenticationFilter and AuthenticationController can be created and some classes modified

--- src/main/java/org/example/fuel_management_system/service/JwtService.java
+++ src/main/java/org/example/fuel_management_system/service/JwtService.java
@@ -40,25 +40,25 @@ public class JwtService {
         return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
     }
 
-        private boolean isTokenExpired(String token) {
+        public boolean isTokenExpired(String token) {
         return extractExpiration(token).before(new Date());
     }
 
-    private Date extractExpiration(String token) {
+    public Date extractExpiration(String token) {
         return extractClaim(token,Claims::getExpiration);
     }
 
-    private String extractUsername(String token) {
+    public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
     }
 
-    private <T> T extractClaim(String token, Function<Claims,T>resolver) {
+    public  <T> T extractClaim(String token, Function<Claims,T>resolver) {
         Claims claims=extractAllClaims(token);
         return resolver.apply(claims);
 
     }
 
-    private Claims extractAllClaims(String token) {
+    public Claims extractAllClaims(String token) {
         return Jwts
                 .parser()
                 .verifyWith(getSignKey())
