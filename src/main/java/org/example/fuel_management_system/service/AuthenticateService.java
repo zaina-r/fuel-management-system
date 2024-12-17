98dc8c4d2cb7878e8db918cffdc9058ad117cd41
holibhai <mohanathasholins17@gmail.com>
2024-12-17 16:34:53 +0530

    created AuthenticationService class update getter setter to user account class and created AuthenticateResponse class

---
+++ src/main/java/org/example/fuel_management_system/service/AuthenticateService.java
@@ -0,0 +1,52 @@
+package org.example.fuel_management_system.service;
+
+import org.example.fuel_management_system.Repository.UserAccountRepository;
+import org.example.fuel_management_system.model.AuthenticationResponse;
+import org.example.fuel_management_system.model.UserAccount;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticateService {
+
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+    private final UserAccountRepository userAccountRepository;
+
+    public AuthenticateService(UserAccountRepository userAccountRepositoryrepository, UserAccountRepository userAccountRepository, PasswordEncoder passwordEncoder, JwtService jwtService, AuthenticationManager authenticationManager) {
+        this.userAccountRepository = userAccountRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtService = jwtService;
+        this.authenticationManager = authenticationManager;
+    }
+    public AuthenticationResponse register(UserAccount request){
+        UserAccount userAccount=new UserAccount();
+
+        userAccount.setNIC(request.getNIC());
+        userAccount.setTelno(request.getTelno());
+        userAccount.setFirstname(request.getFirstname());
+        userAccount.setLastname(request.getLastname());
+        userAccount.setUsername(request.getUsername());
+        userAccount.setPassword(passwordEncoder.encode(request.getPassword()));
+
+        userAccount.setRole(request.getRole());
+        userAccount=userAccountRepository.save(userAccount);
+        String token=jwtService.generateToken(userAccount);
+
+        return new AuthenticationResponse(token);
+
+
+    }
+    public AuthenticationResponse authenticate(UserAccount request){
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(),request.getPassword()));
+        String token=jwtService.generateToken(userAccountRepository.findByUsername(request.getUsername()).orElseThrow());
+
+        UserAccount userAccount=userAccountRepository.findByUsername(request.getUsername()).orElseThrow();
+         token=jwtService.generateToken(userAccount);
+        return new AuthenticationResponse(token);
+    }
+
+}
