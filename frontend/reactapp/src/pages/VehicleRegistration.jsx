156c82f99b3f8c631342c9e87d7fa6366e2b61c3
TharinduThejan <141844415+TharinduThejan@users.noreply.github.com>
2025-01-18 04:22:39 +0530

    Update VehicleRegistration.jsx

--- frontend/reactapp/src/pages/VehicleRegistration.jsx
+++ frontend/reactapp/src/pages/VehicleRegistration.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import './styles.css';
+import './VehicleRegistration_style.css';
 
 const VehicleRegistrationForm = () => {
   const [formData, setFormData] = useState({
