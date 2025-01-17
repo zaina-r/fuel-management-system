b3d9eafea4c1c22ac01394eee1028c063f96b34e
holibhai <146805057+holibhai@users.noreply.github.com>
2025-01-18 01:26:56 +0530

    configuration setting

--- frontend/reactapp/src/App.jsx
+++ frontend/reactapp/src/App.jsx
@@ -1,12 +1,28 @@
 import { useState } from "react";
 import "./App.css";
 import UserLogin from "./pages/UserLogin";
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
-    <div>
-      <UserLogin />
-    </div>
+    // <div>
+    //   <UserLogin />
+    // </div>
+    <BrowserRouter>
+         <div className="fuelPass">
+               <Navbar/>
+               <div className="content">
+                  <Routes>
+                     {/* <Route path="/" element={<UserLogin />} /> */}
+                  </Routes>
+ 
+               </div>
+         </div>
+    
+    </BrowserRouter>
   );
 }
 
