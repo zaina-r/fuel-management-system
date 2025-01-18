3de64a5cdbdc68a35c7a4901634b31e1bb930abf
TharinduThejan <141844415+TharinduThejan@users.noreply.github.com>
2025-01-19 03:57:26 +0530

    Update App.jsx

--- frontend/reactapp/src/App.jsx
+++ frontend/reactapp/src/App.jsx
@@ -4,7 +4,6 @@ import UserLogin from "./pages/UserLogin";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import UserRegisterationForm from "./pages/UserRegisterationForm";
-import VehicleRegistration from "./pages/VehicleRegistration";
 
 import Navbar from "./components/Navbar";
 
@@ -19,7 +18,7 @@ function App() {
               <Route exact path="/" element={<Home />} />
               <Route exact path="/login" element={<UserLogin />} />
               <Route path="/register" element={<UserRegisterationForm />} />
-              <Route path="/register" element={<VehicleRegistration />} />
+         
               
 
             </Routes>
