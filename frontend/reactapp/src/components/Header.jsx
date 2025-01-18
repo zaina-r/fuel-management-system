f4003542539275fcaa2659b809acb2d7ee3c1e88
TharinduThejan <141844415+TharinduThejan@users.noreply.github.com>
2025-01-19 03:52:01 +0530

    edit frontend codes

--- frontend/reactapp/src/components/Header.jsx
+++ frontend/reactapp/src/components/Header.jsx
@@ -4,20 +4,7 @@ import './Header_style.css';
 function Header() {
   return (
     <>
-    <div className="logo">Fuel Management System</div>
-    <div className="menu-toggle">
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>
-    <nav id="navMenu">
-      <a href="#dashboard">Dashboard</a>
-      <a href="#fuel-stock">Fuel Stock</a>
-      <a href="#transactions">Transactions</a>
-      <a href="#reports">Reports</a>
-      <a href="#settings">Settings</a>
-      <a href="#logout">Logout</a>
-    </nav>
+   
     </>
   )
 }
