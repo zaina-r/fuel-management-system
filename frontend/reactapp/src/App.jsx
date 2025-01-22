c84a247ac19f8c771c32ee43101c80ce7280ccbb
Kenuja Sivakumar <sivakum-se21050@stu.kln.ac.lk>
2025-01-22 08:31:59 +0530

    about page related files

--- frontend/reactapp/src/App.jsx
+++ frontend/reactapp/src/App.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home";
 import VehicleRegistration from "./pages/VehicleRegistration";
 import Navbar from './components/Navbar';
-import Navbar2 from './components/Navbar2';
+
 
 
 function App() {
