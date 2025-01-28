6de38f32e59020705463d5e65be0a3b3eb863575
holibhai <146805057+holibhai@users.noreply.github.com>
2025-01-28 19:13:15 +0530

    usestate handle

--- mobileScanner/app/scanner/index.tsx
+++ mobileScanner/app/scanner/index.tsx
@@ -14,3 +14,12 @@ import {
 import { Camera, CameraView } from "expo-camera";
 import axios from "axios";
 import { useRouter } from "expo-router";
+
+export default function QRScanner() {
+  const qrLock = useRef(false); 
+  const [qrdata, setQrData] = useState<string | null>(null);
+  const [rowData, setRowData] = useState<any>(null);
+  const [hasPermission, setHasPermission] = useState<boolean | null>(null);
+  const [loading, setLoading] = useState(false); 
+  const [fuelAmount, setFuelAmount] = useState(""); 
+  const router = useRouter();
