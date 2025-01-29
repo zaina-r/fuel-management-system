63c8864fb6e28392297c574f75cad58eb059901f
holibhai <146805057+holibhai@users.noreply.github.com>
2025-01-29 20:01:14 +0530

    created usestates

--- mobileScanner/app/index.tsx
+++ mobileScanner/app/index.tsx
@@ -4,3 +4,12 @@ import { useCameraPermissions } from "expo-camera";
 import { useState } from "react";
 import axios from "axios";
 import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export default function Home() {
+  const [permission, requestPermission] = useCameraPermissions();
+  const [code, setCode] = useState("");  
+  const [checked,setChekced]=useState(false);
+  const [stationName,setStationName]=useState(""); 
+  const [stationId,setStationId]=useState("");
+  const [fuel_station_Id,setFuel_Station_Id]=useState("");
+  const isPermissionGranted = Boolean(permission?.granted);
