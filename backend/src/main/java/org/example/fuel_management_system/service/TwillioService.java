53a492ef8e763f86b407b5361f74407f7116cedb
Jeyaruban jenushan <jjenushan550@gmail.com>
2025-01-30 22:49:50 +0530

    twillio backend update

--- backend/src/main/java/org/example/fuel_management_system/service/TwillioService.java
+++ backend/src/main/java/org/example/fuel_management_system/service/TwillioService.java
@@ -1,6 +1,12 @@
 //package org.example.fuel_management_system.service;
 //
+//import com.twilio.type.PhoneNumber;
 //import jakarta.annotation.PostConstruct;
+//import org.example.fuel_management_system.DTO.Response;
+//import org.example.fuel_management_system.OtpGenerator.GenerateOtp;
+//import org.example.fuel_management_system.Repository.OTPRecordRepository;
+//import org.example.fuel_management_system.model.OTPRecord;
+//import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.stereotype.Service;
 //
 //
@@ -8,41 +14,76 @@
 //import com.twilio.Twilio;
 //import com.twilio.rest.api.v2010.account.Message;
 //import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.stereotype.Service;
-//import org.springframework.web.bind.annotation.PostMapping;
+//
+//import java.util.HashMap;
+//import java.util.Optional;
+//import java.util.Random;
+//
 //
 //@Service
 //public class TwillioService {
-//
-//    @Value("${twilio.account-sid}")
+//    @Autowired
+//    private OTPRecordRepository otpRecordRepository;
+///*
+//    @Value("${twilio.account.sid}")
 //    private String accountSid;
 //
-//    @Value("${twilio.auth-token}")
+//    @Value("${twilio.auth.token}")
 //    private String authToken;
 //
-//    @Value("${twilio.phone-number}")
+//    @Value("${twilio.phone.number}")
 //    private String twilioPhoneNumber;
+//*/
 //
 //
-//    @PostConstruct
-//    public void initTwilio() {
+//
+//
+//    public Response sendOTP(String phoneNumber) {
+//
 //        Twilio.init(accountSid, authToken);
+//
+//        Response response=new Response();
+//        String otp= GenerateOtp.generateOtp();
+//try{
+//    Message.creator(
+//            new PhoneNumber(phoneNumber),
+//            new PhoneNumber(twilioPhoneNumber),
+//            "Your OTP is: " + otp
+//    ).create();
+//
+//    OTPRecord otpRecord=new OTPRecord(phoneNumber,otp);
+//    otpRecordRepository.save(otpRecord);
+//
+//    response.setMessage("OTP Sent Successfully!");
+//    response.setStatusCode(200);
+//
+//
+//}catch (Exception e){
+//    response.setMessage("Failed to send OTP: " + e.getMessage());
+//    response.setStatusCode(500);
+//
+//}
+//return response;
 //    }
 //
-//    public String sendNotification(String toPhoneNumber, String messageBody) {
-//        try {
-//            if (!toPhoneNumber.startsWith("+")) {
-//                toPhoneNumber = "+94" + toPhoneNumber.substring(1); // Assuming UK country code
-//            }
+//    public Response verifyOTP(String phoneNumber, String otp) {
 //
-//            Message message = Message.creator(
-//                    new com.twilio.type.PhoneNumber(toPhoneNumber),
-//                    new com.twilio.type.PhoneNumber(twilioPhoneNumber),
-//                    messageBody
-//            ).create();
-//            return "Notification sent successfully. Message SID: " + message.getSid();
-//        } catch (Exception e) {
-//            return "Failed to send notification: " + e.getMessage();
+//        Twilio.init(accountSid, authToken);
+//        Response response = new Response();
+//        Optional<OTPRecord> optionalRecord = otpRecordRepository.findById(phoneNumber);
+//       // System.out.println(optionalRecord.get());
+//
+//        if (optionalRecord.isPresent() && optionalRecord.get().getOtp().equals(otp)) {
+//            response.setMessage("OTP Verified Successfully");
+//            response.setStatusCode(200);
+//            otpRecordRepository.deleteById(phoneNumber);
+//        } else {
+//            response.setMessage("Invalid OTP. Please try again.");
+//            response.setStatusCode(400);
 //        }
+//        return response;
 //    }
+//
+//
+//
 //}
