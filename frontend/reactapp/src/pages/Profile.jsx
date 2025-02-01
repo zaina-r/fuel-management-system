29ae7eee0302a1828b953e974ac6928269493e06
holibhai <146805057+holibhai@users.noreply.github.com>
2025-02-01 12:07:25 +0530

    profile page design

--- frontend/reactapp/src/pages/Profile.jsx
+++ frontend/reactapp/src/pages/Profile.jsx
@@ -18,7 +18,7 @@ const ProfilePage = () => {
     try {
       const response = await UserAccountApi.getUserDetails();
       setUser(response.userAccountDto);
-      setFormData(response.userAccountDto); // Initialize formData with user data
+      setFormData(response.userAccountDto);
     } catch (err) {
       console.error(err.message || "An unexpected error occurred.");
     }
@@ -35,10 +35,6 @@ const ProfilePage = () => {
   const handleUpdateDetails = async (formData) => {
     try {
       const response = await UserAccountApi.updateUserDetails(formData);
-      if (!formData.firstname || !formData.lastname || !formData.telno) {
-        setError("All fields are required!");
-        return;
-      }
       if (response.statusCode === 200) {
         setFormData(response.userAccountDto);
 
@@ -62,7 +58,7 @@ const ProfilePage = () => {
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-800 text-white py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-md w-full bg-gray-700 rounded-lg shadow-lg p-6">
+      <div className=" w-full  rounded-lg shadow-lg p-6 ">
         {error && <Error error={error} setError={setError} />}
         {success && <Success success={success} setSuccess={setSuccess} />}
         <h2 className="text-2xl font-bold text-center mb-6">Profile Details</h2>
@@ -130,55 +126,68 @@ const ProfilePage = () => {
             </div>
           </form>
         ) : (
-          <div className="space-y-4">
-            <div>
-              <label className="block text-sm font-medium text-gray-200">
-                First Name
-              </label>
-              <p className="mt-1 text-lg">{user.firstname}</p>
-            </div>
+          <>
+            <div className=" flex flex-col items-center">
+              <div className="flex items-center justify-center gap-3">
+                <div className="w-[300px] h-[1px] bg-neutral-400"></div>
+                <div className="w-[100px] h-[100px] border-2 border-neutral-400 rounded-full"></div>
+                <div className="w-[300px] h-[1px] bg-neutral-400"></div>
+              </div>
+              <div className="my-10">
+                <div className="flex justify-between items-center  my-6 ">
+                  <div>
+                    <label className="block text-sm font-medium text-orange-500">
+                      First Name
+                    </label>
+                    <p className="mt-1 text-lg">{user.firstname}</p>
+                  </div>
 
-            <div>
-              <label className="block text-sm font-medium text-gray-200">
-                Last Name
-              </label>
-              <p className="mt-1 text-lg">{user.lastname}</p>
-            </div>
-
-            <div>
-              <label className="block text-sm font-medium text-gray-200">
-                Mobile Number
-              </label>
-              <p className="mt-1 text-lg">{user.telno}</p>
-            </div>
-
-            <div>
-              <label className="block text-sm font-medium text-gray-200">
-                Email
-              </label>
-              <p className="mt-1 text-lg">{user.username}</p>
-            </div>
-
-            {user.role === "FUELSTATION_OWNER" && (
-              <>
-                <div>
-                  <label className="block text-sm font-medium text-gray-200">
-                    License Number
-                  </label>
-                  <p className="mt-1 text-lg">{user.licenseNumber}</p>
+                  <div>
+                    <label className="block text-sm font-medium text-orange-500">
+                      Last Name
+                    </label>
+                    <p className="mt-1 text-lg">{user.lastname}</p>
+                  </div>
                 </div>
-              </>
-            )}
 
-            <div className="mt-6">
-              <button
-                onClick={() => setIsEditing(true)}
-                className="w-full px-4 py-2 text-sm font-medium bg-blue-600 rounded-md hover:bg-blue-700"
-              >
-                Edit Profile
-              </button>
+                <div className="flex justify-between items-center gap-96  ">
+                  <div>
+                    <label className="block text-sm font-medium text-orange-500">
+                      Mobile Number
+                    </label>
+                    <p className="mt-1 text-lg">{user.telno}</p>
+                  </div>
+
+                  <div>
+                    <label className="block text-sm font-medium text-right text-orange-500">
+                      Email
+                    </label>
+                    <p className="mt-1 text-lg">{user.username}</p>
+                  </div>
+                </div>
+
+                {user.role === "stationOwner" && (
+                  <>
+                    <div>
+                      <label className="block text-sm font-medium text-orange-500">
+                        License Number
+                      </label>
+                      <p className="mt-1 text-sm">{user.licenseNumber}</p>
+                    </div>
+                  </>
+                )}
+
+                <div className="mt-6">
+                  <button
+                    onClick={() => setIsEditing(true)}
+                    className="w-full px-4 py-2 text-sm font-medium bg-blue-600 rounded-md hover:bg-blue-700"
+                  >
+                    Edit Profile
+                  </button>
+                </div>
+              </div>
             </div>
-          </div>
+          </>
         )}
       </div>
     </div>
