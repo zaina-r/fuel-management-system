6acc1d5cf0dc56825187865fbdcc80e17bb50272
holibhai <146805057+holibhai@users.noreply.github.com>
2025-02-04 23:25:16 +0530

    add findByFuelName in interface

--- backend/src/main/java/org/example/fuel_management_system/Repository/FuelPriceRepository.java
+++ backend/src/main/java/org/example/fuel_management_system/Repository/FuelPriceRepository.java
@@ -4,6 +4,10 @@ import org.example.fuel_management_system.model.FuelPrice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface FuelPriceRepository extends JpaRepository<FuelPrice,Integer> {
+    List<FuelPrice> findByFuelName(String fuelName);
 }
