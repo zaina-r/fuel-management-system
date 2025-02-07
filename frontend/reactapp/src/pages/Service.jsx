09d4427f61dbba1588899b3d90729320b6acadf7
holibhai <146805057+holibhai@users.noreply.github.com>
2025-02-08 01:20:20 +0530

    service page changes

--- frontend/reactapp/src/pages/Service.jsx
+++ frontend/reactapp/src/pages/Service.jsx
@@ -1,4 +1,11 @@
 import React from "react";
+import { SlideLeft } from "../animation/direction";
+import { SlideRight } from "../animation/direction";
+import { SlideUp } from "../animation/direction";
+import { motion } from "framer-motion";
+
+
+
 
 const services = [
   {
@@ -28,18 +35,27 @@ const Service = () => {
     <div className="bg-gray-900 text-white min-h-screen p-8 ">
         <div className="my-28">
         <div className="text-center mb-12">
-        <h1 className="text-4xl font-bold text-orange-500">Our <span className="text-blue-500">Service</span></h1>
+        <motion.h1
+            variants={SlideUp(0.2)}
+                     initial="hidden"
+                     whileInView={"visible"}
+        className="text-4xl font-bold text-orange-500">Our <span className="text-blue-500">Service</span></motion.h1>
         <p className="mt-2 text-gray-400">Providing efficient fuel management solutions.</p>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
         {services.map((service, index) => (
-          <div key={index} className="bg-gray-800 rounded-xl shadow-lg overflow-hidden transform hover:scale-105 transition duration-300">
+          <motion.div 
+          variants={SlideLeft(0.1*index)}
+          initial="hidden"
+          whileInView={"visible"}
+          
+          key={index} className="bg-gray-800 rounded-xl shadow-lg overflow-hidden transform hover:scale-105 transition duration-300">
             <img src={service.image} alt={service.title} className="w-full h-56 object-cover" />
             <div className="p-6">
               <h2 className="text-2xl font-semibold text-orange-400">{service.title}</h2>
               <p className="text-gray-300 mt-2">{service.description}</p>
             </div>
-          </div>
+          </motion.div>
         ))}
       </div>
         </div>
