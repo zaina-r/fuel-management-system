af2a40af4c1bfc911e8165cbe6ab96889cea6080
Jeyaruban jenushan <jjenushan550@gmail.com>
2025-02-08 19:13:31 +0530

    asdd header

--- frontend/reactapp/src/apiservice/VehicleApi.jsx
+++ frontend/reactapp/src/apiservice/VehicleApi.jsx
@@ -15,7 +15,10 @@ export default class VehicleApi {
     const userId = localStorage.getItem("userId");
     const response = await axios.post(
       `${this.BASE_URL}/api/verifyAndAddVehicle/${userId}/${fuelAmount}`,
-      vehicleDetails
+      vehicleDetails,
+      {
+        headers: this.getHeader(),
+      }
     );
     return response.data;
   }
@@ -23,48 +26,63 @@ export default class VehicleApi {
   static async getVehicleDetails() {
     const userId = localStorage.getItem("userId");
     const response = await axios.get(
-      `${this.BASE_URL}/api/allVehicleDetails/${userId}`
+      `${this.BASE_URL}/api/allVehicleDetails/${userId}`,
+      {
+        headers: this.getHeader(),
+      }
     );
     return response.data;
   }
 
   static async getVehicleTypes() {
-    const response = await axios.get(`${this.BASE_URL}/api/vehicleTypes`);
+    const response = await axios.get(`${this.BASE_URL}/api/vehicleTypes`, {
+      headers: this.getHeader(),
+    });
     return response.data;
   }
   static async findVehicle(vehicleType) {
     const response = await axios.get(
-      `${this.BASE_URL}/api/vehicle/${vehicleType}`
+      `${this.BASE_URL}/api/vehicle/${vehicleType}`,
+      {
+        headers: this.getHeader(),
+      }
     );
     return response.data;
   }
 
   static async getAvailableVehicles() {
-    const response = await axios.get(`${this.BASE_URL}/api/vehicleTypes`);
+    const response = await axios.get(`${this.BASE_URL}/api/vehicleTypes`, {
+      headers: this.getHeader(),
+    });
     return response.data;
   }
   static async deleteAvailableVehicle(id) {
     const response = await axios.delete(
-      `${this.BASE_URL}/api/vehicleType/delete/${id}`
-    );
-    return response.data;
-  }
-  static async updateAvailableVehicle(id,formData) {
-    const response = await axios.put(
-      `${this.BASE_URL}/api/vehicleTypes/update/${id}`,
-      formData,
+      `${this.BASE_URL}/api/vehicleType/delete/${id}`,
       {
-        headers: {
-          "Content-Type": "application/json",
-        },
+        headers: this.getHeader(),
       }
     );
     return response.data;
-  
-}
+  }
+  static async updateAvailableVehicle(id, formData) {
+    const response = await axios.put(
+      `${this.BASE_URL}/api/vehicleTypes/update/${id}`,
+      formData,
+
+      {
+        headers: this.getHeader(),
+      }
+    );
+    return response.data;
+  }
   static async addAvailableVehicle(vehicle) {
     const response = await axios.post(
-      `${this.BASE_URL}/api/vehicleTypes/add`,vehicle
+      `${this.BASE_URL}/api/vehicleTypes/add`,
+      vehicle,
+      {
+        headers: this.getHeader(),
+      }
     );
     return response.data;
   }
